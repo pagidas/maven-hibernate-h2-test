44e3a07ef85cc7f0d4a6c2a5d9823963aa4a2e77
pagidas <pagakrivos@gmail.com>
2019-06-04 23:40:18 +0100

    Implements insert student functionality

--- src/test/java/dev/kostasakrivos/test/TestHibernateStudentCRUD.java
+++ src/test/java/dev/kostasakrivos/test/TestHibernateStudentCRUD.java
@@ -1,10 +1,16 @@
 package dev.kostasakrivos.test;
 
+import dev.kostasakrivos.test.entity.Student;
 import dev.kostasakrivos.test.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
 
 public class TestHibernateStudentCRUD {
 
@@ -21,6 +27,33 @@ public class TestHibernateStudentCRUD {
     public static void teardownTest() {
         // closes the session factory
         HibernateUtil.shutdown();
+    }
+
+    @Test
+    public void testCreateUser() {
+        // creates a Student
+        System.out.println("\nCreating a new Student...");
+        Student student = new Student("test-firstName", "test-lastName");
+
+        // creates a Session
+        session = factory.openSession();
+
+        // starts a Transaction
+        session.beginTransaction();
+
+        // saves the Student
+        System.out.println("Saving the Student...");
+        session.save(student);
+
+        // commits the Transaction
+        session.getTransaction().commit();
+        System.out.println("Done!\n");
+
+        System.out.println("Looking for that Student...");
+
+        // SELECT that student
+        assertThat(session.get(Student.class, 1), not(nullValue()));
+        System.out.println("Done!\n");
     }
 
 }
