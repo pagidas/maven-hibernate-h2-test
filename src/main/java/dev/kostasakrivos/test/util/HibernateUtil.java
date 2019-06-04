6d7c8837e626213a8001da6885dcdbe88130d7a7
pagidas <pagakrivos@gmail.com>
2019-06-04 23:29:09 +0100

    Implements getProperties()

--- src/main/java/dev/kostasakrivos/test/util/HibernateUtil.java
+++ src/main/java/dev/kostasakrivos/test/util/HibernateUtil.java
@@ -3,12 +3,29 @@ package dev.kostasakrivos.test.util;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+
 public class HibernateUtil {
 
     private static StandardServiceRegistry configureServiceRegistry() {
         return new StandardServiceRegistryBuilder()
                 .applySettings(getProperties())
                 .build();
+    }
+
+    private static Properties getProperties() {
+        Properties properties = new Properties();
+
+        try {
+            properties.load(new FileInputStream("src/main/resources/hibernate.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return properties;
     }
 
 }
