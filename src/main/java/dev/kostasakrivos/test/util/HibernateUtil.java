cea1789ddc4b06b335393d62c12d39cd9389b25c
pagidas <pagakrivos@gmail.com>
2019-06-05 10:54:50 +0100

    Adds documentation to HibernateUtil functionality

--- src/main/java/dev/kostasakrivos/test/util/HibernateUtil.java
+++ src/main/java/dev/kostasakrivos/test/util/HibernateUtil.java
@@ -10,11 +10,26 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+/**
+ * Bootstraps Hibernate functionality and configuration using only programmatic style.
+ *
+ * Calling {@link #getSessionFactory()} and, after transactions are committed, {@link #shutdown()}
+ * will be just enough to get Hibernate up-and-running.
+ */
 public class HibernateUtil {
 
     private static StandardServiceRegistry registry;
     private static SessionFactory sessionFactory;
 
+    /**
+     * Creates the single instance of SessionFactory if there not already one.
+     *
+     * - Creates new MetadataSources using a configured StandardServiceRegistry
+     * - Adds Annotated classes in the MetadataSources for ORM functionality
+     * - Finally builds the SessionFactory
+     *
+     * @return The configured and built SessionFactory instance.
+     */
     public static SessionFactory getSessionFactory() {
 
         if(sessionFactory == null) {
@@ -37,12 +52,24 @@ public class HibernateUtil {
         return sessionFactory;
     }
 
+    /**
+     * Builds and configures the StandardServiceRegistry, parsing a hibernate.properties file,
+     * in order to be create a new instance of MetadataSources.
+     *
+     * @return The configured and built StandardServiceRegistry.
+     */
     private static StandardServiceRegistry configureServiceRegistry() {
         return new StandardServiceRegistryBuilder()
                 .applySettings(getProperties())
                 .build();
     }
 
+    /**
+     * Parses and loads the hibernate.properties file from a specified
+     * path in the project directory, into a Properties instance.
+     *
+     * @return The Properties instance, containing the hibernate.properties data.
+     */
     private static Properties getProperties() {
         Properties properties = new Properties();
 
@@ -55,10 +82,22 @@ public class HibernateUtil {
         return properties;
     }
 
+    /**
+     * Adds annotated classes into a given MetadataSource.
+     *
+     * This where all the POJO entities with JPA annotations are hardcoded,
+     * so that the logic behind creating a SessionFactory is decluttered.
+     *
+     * @param metadataSources The MetadataSources instance which will contain the annotated classes
+     */
     private static void addEntityClasses(MetadataSources metadataSources) {
         metadataSources.addAnnotatedClass(Student.class);
     }
 
+    /**
+     * Destroys the StandardServiceRegistry with the hibernate.properties,
+     * thus destroying the SessionFactory.
+     */
     public static void shutdown() {
         if(registry != null)
             StandardServiceRegistryBuilder.destroy(registry);
